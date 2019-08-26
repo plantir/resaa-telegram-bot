4cad416be187b6918ab71afd38f3f3d2ee0344fb
arminkheirkhahan <arminkheirkhahan@gmail.com>
2019-08-26 17:41:45 +0430

    change

--- Model/Doctor.js
+++ Model/Doctor.js
@@ -1,146 +1,157 @@
 // const dbConf = require('../config/db.config');
 // const redis = dbConf.redis;
-const request = require('request-promise')
+const request = require('request-promise');
 const bot_token =
-  'DG0RIQVKTTKCUEUGURNGOHBLWULTSSQFHISIFXGXDACBMGZFWKDWNBLZKQLFSJDY'
-const dbConf = require('../config/db.config')
-const redis = dbConf.redis
-const _ = require('lodash')
+  'DG0RIQVKTTKCUEUGURNGOHBLWULTSSQFHISIFXGXDACBMGZFWKDWNBLZKQLFSJDY';
+const dbConf = require('../config/db.config');
+const redis = dbConf.redis;
+const _ = require('lodash');
 class Doctor {
   constructor() {
-    this.API_URL = process.env.NODE_ENV === 'development' ? 'https://webapi.resaa.net' : 'http://resa-web-api.bsn.local';
-    this.fields = 'subscriberNumber,firstName,lastName,currentlyAvailable'
+    this.API_URL =
+      process.env.NODE_ENV === 'development'
+        ? 'https://webapi.resaa.net'
+        : 'http://resa-web-api.bsn.local';
+    this.fields = 'subscriberNumber,firstName,lastName,currentlyAvailable';
   }
 
-  static get_doctors({
-    limit = 20,
-    offset = 0,
-    specialtyId,
-    code,
-    name
-  }) {
-    let model = new Doctor()
-    let uri = `${model.API_URL}/Doctors?fields=${
-      model.fields
-      }&limit=1000&offset=${offset}`
+  static get_doctors({ limit = 20, offset = 0, specialtyId, code, name }) {
+    let model = new Doctor();
+    let uri = `${model.API_URL}/Doctors?fields=${model.fields}&limit=1000&offset=${offset}`;
     if (specialtyId) {
-      uri += `&specialtyId=${specialtyId}`
+      uri += `&specialtyId=${specialtyId}`;
     }
     if (code) {
-      uri += `&code=${code}`
+      uri += `&code=${code}`;
     }
     if (name) {
-      uri += `&name=${name}`
+      uri += `&name=${name}`;
     }
     return new Promise((resolve, reject) => {
-
       request({
         method: 'GET',
         json: true,
         uri: encodeURI(uri)
-      }).then(res => {
-        let available = res.result.doctors.filter(item => item.currentlyAvailable);
-        let notavailable = res.result.doctors.filter(item => !item.currentlyAvailable);
-        let doctors = _.sampleSize(available, limit)
-        if (doctors.length < limit) {
-          doctors.push(..._.sampleSize(notavailable, limit - doctors.length))
-        }
-        resolve(doctors);
-      }).catch(err => reject(err))
-    })
+      })
+        .then(res => {
+          let available = res.result.doctors.filter(
+            item => item.currentlyAvailable
+          );
+          let notavailable = res.result.doctors.filter(
+            item => !item.currentlyAvailable
+          );
+          let doctors = _.sampleSize(available, limit);
+          if (doctors.length < limit) {
+            doctors.push(..._.sampleSize(notavailable, limit - doctors.length));
+          }
+          resolve(doctors);
+        })
+        .catch(err => reject(err));
+    });
   }
   static find(id) {
-    let model = new Doctor()
-    let uri = `${
-      model.API_URL
-      }/Doctors/${id}?fields=id,firstName,lastName,currentlyAvailable,subscriberNumber,specialty,tags,expertise,timetable,title,workplaces&clientTimeZoneOffset=-210`
+    let model = new Doctor();
+    let uri = `${model.API_URL}/Doctors/${id}?fields=id,firstName,lastName,currentlyAvailable,subscriberNumber,specialty,tags,expertise,timetable,title,workplaces&clientTimeZoneOffset=-210`;
     return request({
       method: 'GET',
       json: true,
       uri: uri
-    })
+    });
   }
   static get_speciality_list() {
-
     return new Promise((resolve, reject) => {
       redis.get(`speciality_list`, async (err, specialities) => {
         if (specialities) {
-          return resolve(JSON.parse(specialities))
+          return resolve(JSON.parse(specialities));
         } else {
-          let model = new Doctor()
+          let model = new Doctor();
           let res = await request({
             method: 'GET',
             json: true,
             uri: `${model.API_URL}/Rubika/Doctors/MedicalSpecialties`
-          })
-          redis.set(`speciality_list`, JSON.stringify(res.result.medicalSpecialties))
-          return resolve(res.result.medicalSpecialties)
+          });
+          redis.set(
+            `speciality_list`,
+            JSON.stringify(res.result.medicalSpecialties)
+          );
+          return resolve(res.result.medicalSpecialties);
         }
-      })
-    })
+      });
+    });
   }
   static get_time_price(id, phone) {
-    let model = new Doctor()
+    let model = new Doctor();
     return request({
       method: 'GET',
       json: true,
       uri: `${model.API_URL}/Rubika/Doctors/${id}/communicationquote?patientphonenumber=${phone}`
-    })
+    });
   }
   static image_id(doctor_id) {
-    let model = new Doctor()
+    let model = new Doctor();
     return new Promise((resolve, reject) => {
       redis.get(`doctor_${doctor_id}_image`, (err, file_id) => {
         if (file_id) {
-          return resolve(file_id)
+          return resolve(file_id);
         } else {
           request
             .get(`${model.API_URL}/doctors/${doctor_id}/Image`, {
               encoding: null
             })
             .then(image => {
-              return resolve(image)
+              return resolve(image);
             })
             .catch(err => {
-              reject(err)
-            })
+              reject(err);
+            });
         }
-      })
-    })
+      });
+    });
   }
   static request_test_answer(id, phone) {
     return new Promise((resolve, reject) => {
-
-      let model = new Doctor()
+      let model = new Doctor();
       return request({
         method: 'GET',
         json: true,
-        uri: `${model.API_URL}/Rubika/Doctors/${id}/requestTestAnswer?patientphonenumber=${phone}`
-      }).then(res => {
-        resolve(res)
-      }).catch(err => {
-
-        if (id == 7580) {
-          return resolve({
-            status: 'needMoney',
-            user_charge: 8000,
-            request_price: 15000
-          })
-        }
-        if (id == 7536) {
-          return resolve({
-            status: 'ok',
-            user_charge: 8000,
-            request_price: 15000
-          })
-        }
-        resolve({
-          status: 'needTalk',
-          user_charge: 8000,
-          request_price: 15000
-        })
+        uri: `${model.API_URL}/Doctors/${id}/DiagnosticDocumentsService/Quote?patientPhoneNumber=${phone}`
       })
-    })
+        .then(res => {
+          let status = res.result.quote.status;
+          let request_price = res.result.quote.costPerRequest;
+          switch (status) {
+            case 0:
+              resolve({
+                status: 'ok',
+                request_price: request_price,
+                chat_id: res.result.quote.destinations[0].identifier
+              });
+              break;
+            case 1:
+              resolve({
+                status: 'ServiceUnavailable'
+              });
+              break;
+            case 2:
+              resolve({
+                status: 'needTalk',
+                request_price: request_price
+              });
+              break;
+            case 3:
+              resolve({
+                status: 'needMoney',
+                request_price: request_price
+              });
+              break;
+          }
+        })
+        .catch(err => {
+          reject({
+            err
+          });
+        });
+    });
   }
 }
 
--- state/test_answer.js
+++ state/test_answer.js
@@ -3,150 +3,218 @@ const Doctor = require('../Model/Doctor');
 const bot = require('../bot');
 const bot_token = bot.token;
 const _enum = require('../config/enum');
-const _ = require('lodash')
-const request = require('request-promise')
+const _ = require('lodash');
+const request = require('request-promise');
 bot.onText(/ارسال جواب آزمایش/, async msg => {
-    let message = ''
-    let options = {
-        reply_markup: {
-            keyboard: [],
-            resize_keyboard: true
-        }
+  let message = '';
+  let options = {
+    reply_markup: {
+      keyboard: [],
+      resize_keyboard: true
     }
+  };
 
-    let user = new User(msg.chat.id);
-    let visit_doctor = await user.visit_doctor
-    let phone = await user.phone
-    let doctor = await user.last_visit_doctor
-    user.state = _enum.state.test_answer;
-    let test_answer = await Doctor.request_test_answer(visit_doctor, phone)
-    if (test_answer.status === 'needMoney') {
-        message = `اعتبار فعلی شما ${test_answer.user_charge} تومان میباشد و در خواست شما نیاز به ${test_answer.request_price} تومان شارژ دارد `
-        options.reply_markup.keyboard.push([{
-            text: `شارژ اعتبار رسا`,
-        }])
-        options.reply_markup.keyboard.push([{
-            text: `بازگشت`,
-        }])
-        options.reply_markup.keyboard.push([{
-            text: `بازگشت به خانه`,
-        }])
+  let user = new User(msg.chat.id);
+  let visit_doctor = await user.visit_doctor;
+  let phone = await user.phone;
+  let doctor = await user.last_visit_doctor;
+  user.state = _enum.state.test_answer;
+  let test_answer = await Doctor.request_test_answer(visit_doctor, phone);
+  if (test_answer.status === 'needMoney') {
+    message = `اعتبار فعلی شما ${test_answer.user_charge} تومان میباشد و در خواست شما نیاز به ${test_answer.request_price} تومان شارژ دارد `;
+    options.reply_markup.keyboard.push([
+      {
+        text: `شارژ اعتبار رسا`
+      }
+    ]);
+    options.reply_markup.keyboard.push([
+      {
+        text: `بازگشت`
+      }
+    ]);
+    options.reply_markup.keyboard.push([
+      {
+        text: `بازگشت به خانه`
+      }
+    ]);
 
+    return bot.sendMessage(msg.chat.id, message, options);
+  } else if (test_answer.status === 'needTalk') {
+    user.state = _enum.state.doctor_detail;
+    message = `برای ارسال جواب آزمایش نیاز به هماهنگی قبلی با پزشک هست.\nشما در ۲۴ ساعت اخیر با این پزشک مکالمه ای نداشته اید لطفا ابتدا با پزشک خود مکالمه کنید سپس جواب آزمایش را ارسال نمایید`;
+    options.reply_markup.keyboard.push([
+      {
+        text: `تماس با دکتر ${doctor.firstName} ${doctor.lastName}`
+      }
+    ]);
+    options.reply_markup.keyboard.push([
+      {
+        text: `بازگشت`
+      }
+    ]);
+    options.reply_markup.keyboard.push([
+      {
+        text: `بازگشت به خانه`
+      }
+    ]);
 
-        return bot.sendMessage(msg.chat.id, message, options)
-    } else if (test_answer.status === 'needTalk') {
-        user.state = _enum.state.doctor_detail
-        message = `برای ارسال جواب آزمایش نیاز به هماهنگی قبلی با پزشک هست.\nشما در ۲۴ ساعت اخیر با این پزشک مکالمه ای نداشته اید لطفا ابتدا با پزشک خود مکالمه کنید سپس جواب آزمایش را ارسال نمایید`
-        options.reply_markup.keyboard.push([{
-            text: `تماس با دکتر ${doctor.firstName} ${doctor.lastName}`,
-        }])
-        options.reply_markup.keyboard.push([{
-            text: `بازگشت`,
-        }])
-        options.reply_markup.keyboard.push([{
-            text: `بازگشت به خانه`,
-        }])
-
-
-        return bot.sendMessage(msg.chat.id, message, options)
+    return bot.sendMessage(msg.chat.id, message, options);
+  }
+  message = `هزینه جواب آزمایش ${test_answer.request_price} تومان می باشد و در صورت ارسال فایل از شارژ رسا شما کم میشود\nدر صورت تایید عکس آزمایش خود را بفرستید`;
+  options.reply_markup.keyboard.push([
+    {
+      text: `بازگشت`
     }
-    message = `هزینه جواب آزمایش ${test_answer.request_price} تومان می باشد و در صورت ارسال فایل از شارژ رسا شما کم میشود\nدر صورت تایید عکس آزمایش خود را بفرستید`
-    options.reply_markup.keyboard.push([{
-        text: `بازگشت`,
-    }])
-    options.reply_markup.keyboard.push([{
-        text: `بازگشت به خانه`,
-    }])
-    bot.sendMessage(msg.chat.id, message, options)
-})
+  ]);
+  options.reply_markup.keyboard.push([
+    {
+      text: `بازگشت به خانه`
+    }
+  ]);
+  bot.sendMessage(msg.chat.id, message, options);
+});
 bot.onText(/حذف تمامی فایل ها و ارسال مجدد/, async msg => {
-    let user = new User(msg.chat.id)
-    let state = await user.state;
-    if (state != _enum.state.test_answer) {
-        return
+  let user = new User(msg.chat.id);
+  let state = await user.state;
+  if (state != _enum.state.test_answer) {
+    return;
+  }
+  await user.remove_files();
+  let message = `همه فایل های ارسال شده پاک شده لطفا فایل خود رو مجددا ارسال نمایید`;
+  let options = {
+    reply_markup: {
+      keyboard: [],
+      resize_keyboard: true
     }
-    await user.remove_files()
-    let message = `همه فایل های ارسال شده پاک شده لطفا فایل خود رو مجددا ارسال نمایید`
-    let options = {
-        reply_markup: {
-            keyboard: [],
-            resize_keyboard: true
-        }
+  };
+  options.reply_markup.keyboard.push([
+    {
+      text: `بازگشت به خانه`
     }
-    options.reply_markup.keyboard.push([{
-        text: `بازگشت به خانه`,
-    }])
+  ]);
 
-    return bot.sendMessage(msg.chat.id, message, options)
-})
+  return bot.sendMessage(msg.chat.id, message, options);
+});
 bot.on('photo', async msg => {
-    let user = new User(msg.chat.id)
-    let state = await user.state;
-    if (state != _enum.state.test_answer) {
-        return
+  let user = new User(msg.chat.id);
+  let state = await user.state;
+  if (state != _enum.state.test_answer) {
+    return;
+  }
+  let { file_id } = msg.photo.reverse()[0];
+  let {
+    result: { file_path }
+  } = await request.get({
+    url: `https://api.telegram.org/bot${bot_token}/getFile?file_id=${file_id}`,
+    json: true
+  });
+  let files = await user.add_file(
+    `https://api.telegram.org/file/bot${bot_token}/${file_path}`
+  );
+  let message = `شما تا کنون ${files.length} فایل پیوست کرده اید اگر فایل دیگری هم دارید ارسال کنید در غیر اینصورت بر روی دکمه ارسال جواب آزمایش ضربه بزنید`;
+  let options = {
+    reply_markup: {
+      keyboard: [],
+      resize_keyboard: true
     }
-    let {
-        file_id
-    } = msg.photo.reverse()[0]
-    let {
-        result: {
-            file_path
-        }
-    } = await request.get({
-        url: `https://api.telegram.org/bot${bot_token}/getFile?file_id=${file_id}`,
-        json: true
-    })
-    let files = await user.add_file(`https://api.telegram.org/file/bot${bot_token}/${file_path}`)
-    let message = `شما تا کنون ${files.length} فایل پیوست کرده اید اگر فایل دیگری هم دارید ارسال کنید در غیر اینصورت بر روی دکمه ارسال جواب آزمایش ضربه بزنید`
-    let options = {
-        reply_markup: {
-            keyboard: [],
-            resize_keyboard: true
-        }
+  };
+  options.reply_markup.keyboard.push([
+    {
+      text: `اتمام`
     }
-    options.reply_markup.keyboard.push([{
-        text: `اتمام`,
-    }])
-    options.reply_markup.keyboard.push([{
-        text: `حذف تمامی فایل ها و ارسال مجدد`,
-    }])
-    options.reply_markup.keyboard.push([{
-        text: `بازگشت به خانه`,
-    }])
-    return bot.sendMessage(msg.chat.id, message, options)
-
-})
+  ]);
+  options.reply_markup.keyboard.push([
+    {
+      text: `حذف تمامی فایل ها و ارسال مجدد`
+    }
+  ]);
+  options.reply_markup.keyboard.push([
+    {
+      text: `بازگشت به خانه`
+    }
+  ]);
+  return bot.sendMessage(msg.chat.id, message, options);
+});
 bot.onText(/اتمام|تلاش مجدد/, async msg => {
-    let user = new User(msg.chat.id);
-    let {
-        subscriberNumber
-    } = await user.last_visit_doctor
-    let res = await Doctor.find(subscriberNumber)
-    let doctor = res.result.doctor;
-
-    let message;
-    let options = {
-        reply_markup: {
-            keyboard: [],
-            resize_keyboard: true
-        }
+  let user = new User(msg.chat.id);
+  let phone = await user.phone;
+  let doctor_id = await user.visit_doctor;
+  // phone = '09356659943';
+  // doctor_id = 6843;
+  let res = await Doctor.find(doctor_id);
+  let doctor = res.result.doctor;
+  let message;
+  let options = {
+    reply_markup: {
+      keyboard: [],
+      resize_keyboard: true
     }
-    try {
-        let tracking_code = await user.send_testAnswer(38320614)
-        message = `جواب آزمایش شما با موفقیت برای دکتر ${doctor.firstName} ${doctor.lastName} ارسال شد\n کد پیگیری جواب آزمایش شما ${tracking_code}`
-        options.reply_markup.keyboard.push([{
-            text: `بازگشت به خانه`,
-        }])
-    } catch (error) {
-        message = error;
-        options.reply_markup.keyboard.push([{
-            text: `تلاش مجدد`,
-        }])
-        options.reply_markup.keyboard.push([{
-            text: `بازگشت به خانه`,
-        }])
+  };
+  try {
+    let test_answer = await Doctor.request_test_answer(doctor_id, phone);
+    let { tracking_code, count } = await user.send_testAnswer(
+      test_answer.chat_id
+    );
+    message = `جواب آزمایش شما با موفقیت برای دکتر ${doctor.firstName} ${doctor.lastName} ارسال شد\n کد پیگیری جواب آزمایش شما ${tracking_code}`;
+    options.reply_markup.keyboard.push([
+      {
+        text: `بازگشت به خانه`
+      }
+    ]);
+    user.confirm_testAnswer(doctor_id, tracking_code, count);
+  } catch (error) {
+    message = error;
+    message = error;
+    options.reply_markup.keyboard.push([
+      {
+        text: `تلاش مجدد`
+      }
+    ]);
+    options.reply_markup.keyboard.push([
+      {
+        text: `بازگشت به خانه`
+      }
+    ]);
+  }
 
-    }
+  bot.sendMessage(msg.chat.id, message, options);
+  //   let user = new User(msg.chat.id);
+  //   let { subscriberNumber } = await user.last_visit_doctor;
+  //   let res = await Doctor.find(subscriberNumber);
+  //   let doctor = res.result.doctor;
 
-    bot.sendMessage(msg.chat.id, message, options)
-})
+  //   let message;
+  //   let options = {
+  //     reply_markup: {
+  //       keyboard: [],
+  //       resize_keyboard: true
+  //     }
+  //   };
+  //   try {
+  //     let test_answer = await Doctor.request_test_answer(doctor_id, phone);
+  //     let { tracking_code, count } = await user.send_testAnswer(
+  //       test_answer.chat_id
+  //     );
+  //     let tracking_code = await user.send_testAnswer(38320614);
+  //     message = `جواب آزمایش شما با موفقیت برای دکتر ${doctor.firstName} ${doctor.lastName} ارسال شد\n کد پیگیری جواب آزمایش شما ${tracking_code}`;
+  //     options.reply_markup.keyboard.push([
+  //       {
+  //         text: `بازگشت به خانه`
+  //       }
+  //     ]);
+  //   } catch (error) {
+  //     message = error;
+  //     options.reply_markup.keyboard.push([
+  //       {
+  //         text: `تلاش مجدد`
+  //       }
+  //     ]);
+  //     options.reply_markup.keyboard.push([
+  //       {
+  //         text: `بازگشت به خانه`
+  //       }
+  //     ]);
+  //   }
+
+  //   bot.sendMessage(msg.chat.id, message, options);
+});
